67b29b5064966a3ee6cf249076d8e4f53cb674a8
Simon Hohenwarter <113617179+simonh1701@users.noreply.github.com>
2023-05-12 18:17:37 +0200

    Improve SEO by adding metatags and add favicon

--- layouts/layout.js
+++ layouts/layout.js
@@ -1,11 +1,16 @@
 import Head from "next/head";
 import Footer from "components/footer";
 import Navbar from "components/navbar";
+import { useRouter } from "next/router";
 
 export default function Layout({ children, ...customMeta }) {
+  const router = useRouter();
   const meta = {
     title: "Sortify",
-    description: "",
+    description:
+      "Sort your Spotify playlists by attributes like name, artist, release date, duration, popularity and many more ...",
+    image: "https://sortify.simonh1701.app/images/opengraph-image.png",
+    type: "website",
     ...customMeta,
   };
 
@@ -13,6 +18,27 @@ export default function Layout({ children, ...customMeta }) {
     <>
       <Head>
         <title>{meta.title}</title>
+        <meta name="title" content={meta.title} />
+        <meta name="description" content={meta.description} />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`https://sortify.simonh1701.app${router.asPath}`}
+        />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.image} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@simonh1701" />
+        <meta
+          property="twitter:url"
+          content={`https://sortify.simonh1701.app${router.asPath}`}
+        />
+        <meta property="twitter:title" content={meta.title} />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:image" content={meta.image} />
       </Head>
       <div className="flex min-h-screen flex-col">
         <Navbar></Navbar>
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="favicon/manifest.webmanifest" />
+        <link rel="icon" href="favicon/favicon.ico" sizes="any" />
+        <link rel="icon" href="favicon/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="favicon/apple-touch-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
