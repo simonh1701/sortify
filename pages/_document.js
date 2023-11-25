6aec8a0960d1003d9daba4b86af6e2754853b261
Simon Hohenwarter <113617179+simonh1701@users.noreply.github.com>
2023-11-25 12:38:43 +0100

    Change/Fix link tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="favicon/manifest.webmanifest" />
-        <link rel="icon" href="favicon/favicon.ico" sizes="any" />
-        <link rel="icon" href="favicon/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/manifest.webmanifest" />
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
       </Head>
       <body>
         <Main />
